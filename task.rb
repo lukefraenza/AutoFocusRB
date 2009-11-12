953f3cf25e5c8e2f9bf8acdfcc2fdc5d46f65b5d
Luke Fraenza <luke.fraenza@gmail.com>
2009-11-12 15:56:25 -0500

    Rethinking how tasks and lists should be set up
    One list, of many tasks.  List is just an array.
    Tasks themselves hold if they're in open, closed or review list.

--- task.rb
+++ task.rb
@@ -1,20 +1,28 @@
 class Task
-  attr_reader :status
-  attr_accessor :name
+  attr_accessor :name, :done, :list, :dismissed
 
   def initialize(name)
     @name = name
-    @status = :todo
+    @done = false
+    @list = :open        # or :closed or :review
+    @dismissed = false
   end
 
   def do
-    @status = :done
+    @done = true
   end
 
+  def dismiss
+    @dismissed = true
+  end
 
   def to_s
-    if @status == :done
+    if @done
         "[X] #{@name}"
+    elsif @dismissed
+        "[D] #{@name}"
+    elsif @list == :review
+        "[R] #{@name}"
     else
         "[ ] #{@name}"
     end
@@ -22,3 +30,45 @@ class Task
 
 end
 
+
+class AFList
+  
+  def initialize
+    @myList = []
+    @mode = :open # or :closed or :review
+  end
+
+  def add(name)
+    task = Task.new(name)
+    @myList << task
+  end
+
+  def to_s
+    @myList.each {|t| puts t}
+  end
+
+  def current
+    #  return current task
+  end
+
+  def do
+    # call do on current task
+  end
+
+  def work
+    # call workon on current task
+  end
+
+  def skip
+    # call skip on current task
+  end
+
+end
+
+af = AFList.new
+af.add "pick up veggies"
+af.add "grill chicken"
+af.add "buy pio"
+puts af
+
+puts 
